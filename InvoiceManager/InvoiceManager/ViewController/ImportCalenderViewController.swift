ed17fc7d3849580cde01efea32e5b269206c51a9
Keiji Suzuki <keiji.suzuki.88801@gmail.com>
2022-06-23 17:24:40 -0700

    Add title and import calender view controller

--- InvoiceManager/InvoiceManager/ViewController/ImportCalenderViewController.swift
+++ InvoiceManager/InvoiceManager/ViewController/ImportCalenderViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImportCalenderViewController.swift
 //  InvoiceManager
 //
 //  Created by 鈴木啓司 on 2022-06-23.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ImportCalenderViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view.
   }
-
-
+  
+  
 }
-
---
+++ InvoiceManager/InvoiceManager/ViewController/TitleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TitleViewController.swift
+//  InvoiceManager
+//
+//  Created by 鈴木啓司 on 2022-06-23.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
