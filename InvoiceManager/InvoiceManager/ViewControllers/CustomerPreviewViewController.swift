d4196b5e900a0e7b03d96deea41b43bf86680c3d
Kaiya Takahashi <yaikashi@icloud.com>
2022-07-11 17:48:35 -0700

    feat: implement CustomerPreviewViewController

---
+++ InvoiceManager/InvoiceManager/ViewControllers/CustomerPreviewViewController.swift
@@ -0,0 +1,190 @@
+//
+//  CustomerPreviewViewController.swift
+//  InvoiceManager
+//
+//  Created by Kaiya Takahashi on 2022-07-11.
+//
+
+import UIKit
+
+class CustomerPreviewViewController: UIViewController {
+
+    let tempEvents: [EventDetails] = [
+        EventDetails(eventStartTime: Date.now, eventEndTime: Date(timeInterval: 60*60*4, since: Date.now), eventName: "Programming class", hourlyWage: 20),
+        EventDetails(eventStartTime: Date.now, eventEndTime: Date(timeInterval: 60*60*4, since: Date.now), eventName: "Football class", hourlyWage: 30),
+        EventDetails(eventStartTime: Date.now, eventEndTime: Date(timeInterval: 60*60*6, since: Date.now), eventName: "Programming class", hourlyWage: 20),
+        EventDetails(eventStartTime: Date.now, eventEndTime: Date(timeInterval: 60*60*4, since: Date.now), eventName: "Algorithm class", hourlyWage: 10),
+        EventDetails(eventStartTime: Date.now, eventEndTime: Date(timeInterval: 60*60*4, since: Date.now), eventName: "Linguistic class", hourlyWage: 15),
+        EventDetails(eventStartTime: Date.now, eventEndTime: Date(timeInterval: 60*60*4, since: Date.now), eventName: "Music class", hourlyWage: 25),
+        EventDetails(eventStartTime: Date.now, eventEndTime: Date(timeInterval: 60*60*4, since: Date.now), eventName: "Baseball class", hourlyWage: 45)
+    ]
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var companyLogo: UIImageView!
+    @IBOutlet var companyNameLabel: UILabel!
+    @IBOutlet var companyAdressLabel: UILabel!
+    @IBOutlet var paymentMethodLabel: UILabel!
+    @IBOutlet var companyEmailLabel: UILabel!
+    @IBOutlet var invoiceIDLabel: UILabel!
+    @IBOutlet var customerNameLabel: UILabel!
+    @IBOutlet var customerEmailLabel: UILabel!
+    @IBOutlet var addItemBtn: UIButton!
+    @IBOutlet var itemCollectionView: UICollectionView!
+    @IBOutlet var totalIncomeLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var sendBtn: UIButton!
+    @IBOutlet var PDFBtn: UIButton!
+
+    var totalIncome: Int = 0
+    var date: Date?
+    var customer: Customer?
+    var userSetting: UserSetting?
+
+    typealias DataSourceType = UICollectionViewDiffableDataSource<ViewModel.Section, ViewModel.Item>
+    var dataSource: DataSourceType!
+    var model = Model()
+
+    enum ViewModel {
+        typealias Section = Int
+
+        typealias Item = EventDetails
+    }
+
+    struct Model {
+        var eventsDetails = [EventDetails]()
+    }
+
+    struct EventDetails: Hashable {
+        let eventStartTime: Date
+        let eventEndTime: Date
+        let eventName: String
+        let hourlyWage: Int
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dataSource = createDataSource()
+        itemCollectionView.collectionViewLayout = createLayout()
+        update()
+        setupInformationView()
+    }
+
+    func setupInformationView() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM"
+        let month: String
+        let customerName = customer?.information.customerName
+        if let date = date, let customerName = customerName {
+            month = dateFormatter.string(from: date)
+            titleLabel.text = "\(customerName)'s Invoice in \(month)"
+        } else {
+            titleLabel.text = "No Data"
+        }
+        // Need to modify
+        // ompanyLogo.image = userSetting?.logoImage ?? UIImage(systemName: "person")
+        companyLogo.image = UIImage(systemName: "person")
+        companyNameLabel.text = userSetting?.companyName ?? "No Data"
+        companyAdressLabel.text = userSetting?.companyAddress ?? "No Data"
+        paymentMethodLabel.text = userSetting?.paymentMethod ?? "No Data"
+        companyEmailLabel.text = userSetting?.eMailAddress ?? "No Data"
+
+        invoiceIDLabel.text = "\(customer?.customerID ?? 0)"
+        customerNameLabel.text = customerName ?? "No Data"
+        customerEmailLabel.text = customer?.information.eMailAddress ?? "No Data"
+        itemCollectionView.backgroundColor = UIColor(red: 247/255, green: 247/255, blue: 247/255, alpha: 1)
+        setupTotalIncome(eventsDetails: tempEvents)
+    }
+
+    func update() {
+        model.eventsDetails = tempEvents
+
+        updateCollectionView()
+    }
+
+    func updateCollectionView() {
+        var sectionID = [ViewModel.Section]()
+        let itemsBySection = model.eventsDetails.reduce(into: [ViewModel.Section: [ViewModel.Item]]()) { partialResult, eventDetails in
+            partialResult[0, default: []].append(eventDetails)
+        }
+
+        sectionID.append(0)
+        dataSource.applySnapshotUsing(sectionIDs: sectionID, itemsBySeciton: itemsBySection)
+    }
+
+    func createDataSource() -> DataSourceType {
+        dataSource = DataSourceType(collectionView: itemCollectionView, cellProvider: { (collectionView, indexPath, itemIdentifier) -> EventDetailsCollectionViewCell in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "EventDetailsCell", for: indexPath) as! EventDetailsCollectionViewCell
+            cell.eventDateLabel.text = self.twoDateIntoString(startDate: itemIdentifier.eventStartTime, endDate: itemIdentifier.eventEndTime)
+            cell.eventNameLabel.text = itemIdentifier.eventName
+            let income = itemIdentifier.hourlyWage * self.calculateTimeDifferenceInHours(startTime: itemIdentifier.eventStartTime, endTime: itemIdentifier.eventEndTime)
+            cell.eventIncomeLabel.text = self.formatAsCurrency(number: income)
+            return cell
+        })
+
+        return dataSource
+    }
+
+    func formatAsCurrency(number: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        return "\(numberFormatter.string(for: number) ?? "")"
+    }
+
+    func calculateTimeDifferenceInHours(startTime: Date, endTime: Date) -> Int {
+        let diffComponents = Calendar.current.dateComponents([.hour], from: startTime, to: endTime)
+        return diffComponents.hour!
+    }
+
+    func setupTotalIncome(eventsDetails: [EventDetails]) {
+        for eventDetails in eventsDetails {
+            totalIncome += eventDetails.hourlyWage * calculateTimeDifferenceInHours(startTime: eventDetails.eventStartTime, endTime: eventDetails.eventEndTime)
+        }
+        totalIncomeLabel.text = formatAsCurrency(number: totalIncome)
+    }
+
+    func twoDateIntoString(startDate: Date, endDate: Date) -> String {
+        let startDateFormatter = DateFormatter()
+        startDateFormatter.dateFormat = "MM/d, HH:mm"
+        let endDateFormatter = DateFormatter()
+        endDateFormatter.dateFormat = "HH:mm"
+        let strDate = "\(startDateFormatter.string(from: startDate))-\(endDateFormatter.string(from: endDate))"
+        return strDate
+    }
+
+    func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(40))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.2))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+
+    @IBAction func sendBtnTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func PDFBtnTapped(_ sender: UIButton) {
+    }
+
+    @IBAction func addItemBtnTapped(_ sender: UIButton) {
+    }
+
+}
+
+extension UICollectionViewDiffableDataSource {
+    func applySnapshotUsing(sectionIDs: [SectionIdentifierType], itemsBySeciton: [SectionIdentifierType: [ItemIdentifierType]], sectionRetainedIfEmpty: Set<SectionIdentifierType> = Set<SectionIdentifierType>()) {
+        applySnapshotUsing(sectionIDs: sectionIDs, itemsBySection: itemsBySeciton, animatingDifferences: true, sectionRetainedIfEmpty: sectionRetainedIfEmpty)
+    }
+
+    func applySnapshotUsing(sectionIDs: [SectionIdentifierType], itemsBySection: [SectionIdentifierType: [ItemIdentifierType]], animatingDifferences: Bool, sectionRetainedIfEmpty: Set<SectionIdentifierType> = Set<SectionIdentifierType>()) {
+        var snapshot = NSDiffableDataSourceSnapshot<SectionIdentifierType, ItemIdentifierType>()
+
+        for sectionID in sectionIDs {
+            guard let sectionItems = itemsBySection[sectionID], sectionItems.count > 0 || sectionRetainedIfEmpty.contains(sectionID) else { continue }
+            snapshot.appendSections([sectionID])
+            snapshot.appendItems(sectionItems, toSection: sectionID)
+        }
+        self.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+}
---
+++ InvoiceManager/InvoiceManager/ViewControllers/EventDetailsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EventDetailsCollectionViewCell.swift
+//  InvoiceManager
+//
+//  Created by Kaiya Takahashi on 2022-07-11.
+//
+
+import UIKit
+
+class EventDetailsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var eventDateLabel: UILabel!
+    @IBOutlet var eventNameLabel: UILabel!
+    @IBOutlet var eventIncomeLabel: UILabel!
+
+}
